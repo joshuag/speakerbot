56aa5aa521ecfa51eef9a617088f5e04211931db
Joshua Gooden <joshuag@gmail.com>
2014-05-23 16:13:48 -0400

    un ahck

--- Speakerbot.py
+++ Speakerbot.py
@@ -135,12 +135,7 @@ class Speakerbot(object):
     @event
     def play(self, name):
 
-        if ".mp3" in name:
-            sound = name
-        else:
-            sound = self.sounds[name][0]
-
-        self.se.play(sound)
+        self.se.play(self.sounds[name][0])
 
 
     def say(self, name="", speech_text=""):
