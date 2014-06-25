9f200b0f9dfecf93c558d41a556da204066383ec
Joshua Gooden <joshuag@gmail.com>
2014-06-25 09:58:37 -0400

    Wrong decorator order

--- Speakerbot.py
+++ Speakerbot.py
@@ -57,8 +57,8 @@ class Speakerbot(PluggableObject):
 
         return self.sounds
 
-    @lock
     @event
+    @lock
     def play(self, name):
         self.se.play(self.sounds[name][0])
 
@@ -66,8 +66,8 @@ class Speakerbot(PluggableObject):
     def speech_provider_say(self, speech_text, record_utterance):
         self.tts.say(speech_text)
 
-    @lock
     @event
+    @lock
     def say(self, speech_text="", record_utterance=False):
 
         token = None
