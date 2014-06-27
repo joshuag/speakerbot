07be7f4a77c064d6fd795a52a703bc61b18849ee
Joshua Gooden <joshuag@gmail.com>
2014-06-27 15:34:57 -0400

    Let's move this to setuptools so that Greg can be happy

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SpeakerBot',
+    version='0.9',
+    long_description="A communal soundboard written in Flask",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['flask', 'requests', 'twython', 'pyquery']
+)
