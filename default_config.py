2ae0f7a8021c733f399b5b34fc05205460a26320
Joshua Gooden <joshuag@gmail.com>
2014-06-27 15:05:42 -0400

    Example config file

---
+++ default_config.py
@@ -0,0 +1,17 @@
+""" This is default config file. Fill in and rename to config.py """
+config = {
+    'mashape_api_key': "",
+    'twitter':{
+        'app_key':'',
+        'app_secret':'',
+        'oauth_token':'',
+        'oauth_token_secret':''
+    },
+    'database':{
+    	'driver':'mysql',
+    	'host':'',
+    	'user':'',
+    	'pass':'',
+    	'database':''
+    }
+}
