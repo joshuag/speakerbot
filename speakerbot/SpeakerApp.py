ffb16ec2656fc99d054394152f1562d4064a21c6
Ross Salupo <ross.salupo@vertical-knowledge.com>
2014-07-02 14:39:40 -0400

    Devastating oversight

--- speakerbot/SpeakerApp.py
+++ speakerbot/SpeakerApp.py
@@ -268,7 +268,7 @@ def say(text=None):
         record_utterance = False
 
     if 'george' in text.lower() and random.randint(1,4) == 1:
-        text = re.sub('george', lambda x:random.choice(['alejandro','alex','dave','eric','gavin','george','gianni','greg','josh','matt','ross','tim','tuan']), text)
+        text = re.sub('george', lambda x:random.choice(['alejandro','alex','dave','dickbutt','eric','gavin','george','gianni','greg','josh','matt','ross','tim','tuan']), text)
 
     sb.say(text, record_utterance=record_utterance)
 
