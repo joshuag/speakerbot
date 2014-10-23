10c657858f31fee0b93892a7f55a3832309d429d
Ross S. <ross.salupo@vertical-knowledge.com>
2014-10-23 12:06:43 -0400

    Update speakonomy.py

--- speakerbot/speakonomy.py
+++ speakerbot/speakonomy.py
@@ -25,7 +25,7 @@ class Speakonomy:
         return False
 
     def deposit_funds(self, amount=1):
-        assert isinstance(amount,long)
+        assert isinstance(amount,int)
         self.db.execute("UPDATE bank_account set balance=balance+{}".format(amount))
 
     def get_free_play_timeout(self, force_check=False):
